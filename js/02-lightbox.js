87aa61fe0819b378712f88e4d0485b3735bb23ca
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-10 21:44:13 +0300

    added task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgGallaryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createImgGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+});
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  showCounter: false,
+});
 
 console.log(galleryItems);
