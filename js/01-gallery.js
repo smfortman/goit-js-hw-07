3ad20d80988b43d6c552bc0d4d784f7386e25161
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-10 18:28:28 +0300

    added solution task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgGallaryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createImgGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="large-image.jpg"
+            alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join('');
+}
 
 console.log(galleryItems);
