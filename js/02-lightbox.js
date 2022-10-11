fa7b5a49d972dc099fe16eed91773ebcca462700
Sergei Ageev <sergei.ageev@gmail.com>
2022-10-11 20:44:52 +0300

    fixed task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,21 +8,17 @@ function createImgGallaryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>`;
+    <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
     })
     .join('');
 }
-
-galleryContainer.addEventListener('click', event => {
-  event.preventDefault();
-});
 
 const lightbox = new SimpleLightbox('.gallery__item', {
   captionsData: 'alt',
   captionDelay: 250,
   showCounter: false,
 });
-
-console.log(galleryItems);
